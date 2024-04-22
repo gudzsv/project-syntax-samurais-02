fa7a0c369dc7e3cf361fed7264f5421f942e8ccd
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-04-23 01:11:00 +0300

    change main js file src

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,10 @@
-import '@partials/header';
-import '@partials/hero';
 import '@partials/about-me';
 import '@partials/benefits';
-import '@partials/projects';
-import '@partials/faq';
 import '@partials/covers';
+import '@partials/faq';
+import '@partials/footer';
+import '@partials/header';
+import '@partials/hero';
+import '@partials/projects';
 import '@partials/reviews';
 import '@partials/work-together';
-import '@partials/footer';
-// import './js/header/header-mobile-menu';
-// import './js/signup/signup-form';
