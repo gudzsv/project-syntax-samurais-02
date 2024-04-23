06ea8a2eb0c92225870aa09aac8e364dd189756f
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-04-23 23:39:22 +0300

    added favicon

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    publicDir: '../public',
     build: {
       sourcemap: true,
 
