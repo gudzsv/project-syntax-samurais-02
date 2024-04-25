fccfc419d3326bee31720cc34123f4ffc80cd878
Milana-Ro <milana.roslyak@gmail.com>
2024-04-25 17:31:11 +0300

    Create markup and main logic

--- src/partials/work-together/work-together.js
+++ src/partials/work-together/work-together.js
@@ -1,1 +1,59 @@
-console.log('WORK-TOGETHER');
+import { sendUserDataApi } from './userDataApi';
+
+const STORAGE_KEY = 'formData';
+
+const formData = getFromLocalStorage() || {
+  userEmail: '',
+  userComments: '',
+};
+
+const form = document.querySelector('.js-submit-form');
+const [inputEmail, inputComments] = form;
+inputEmail.value = formData.userEmail;
+inputComments.value = formData.userComments;
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  setToLocalStorage(formData);
+});
+
+form.addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const { userEmail, userComments } = formData;
+
+  if (!userEmail || !userComments) {
+    return;
+  }
+
+  try {
+    const data = await sendUserDataApi({
+      email: userEmail,
+      comment: userComments,
+    });
+
+    // const { title, message } = data;
+    // TODO open modal window
+
+    resetData();
+  } catch (error) {
+    // izitoast
+  }
+}
+
+// helpers
+function setToLocalStorage(value, key = STORAGE_KEY) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+function getFromLocalStorage(key = STORAGE_KEY) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+function resetData(key = STORAGE_KEY) {
+  localStorage.removeItem(key);
+  form.reset();
+  formData.userEmail = '';
+  formData.userComments = '';
+}
