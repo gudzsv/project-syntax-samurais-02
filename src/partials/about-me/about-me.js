9bb93af85d5777778180160ff3cfa32c30bf9f67
nelli <nelli.diachkina@gmail.com>
2024-04-26 21:54:34 +0300

    Accordion + corrections in icons.svg,css,html.

--- src/partials/about-me/about-me.js
+++ src/partials/about-me/about-me.js
@@ -1,1 +1,18 @@
-console.log('ABOUT-ME');
+import Accordion from 'accordion-js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btnAcc = [...document.querySelectorAll('.about-me-btn')][0];
+  btnAcc.classList.add('btn-active');
+
+  const accAboutMe = new Accordion('#accordionAbout', {
+    openOnInit: [0],
+    beforeOpen: function (accAboutMe) {
+      const btn = accAboutMe.querySelector('.about-me-btn.ac-trigger');
+      btn.classList.add('btn-active');
+    },
+    beforeClose: function (accAboutMe) {
+      const btn = accAboutMe.querySelector('.about-me-btn.ac-trigger');
+      btn.classList.remove('btn-active');
+    },
+  });
+});
